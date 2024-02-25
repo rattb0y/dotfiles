ff92b09b1f3b2dacf427e8c1488556a28659841a
Kris <kris.purchase@proton.me>
2024-02-25 15:52:07 -0500

    updated hyperland config

---
+++ .config/hypr/scripts/wofi-power.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+entries=" Lock\n⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries|wofi --width 250 --height 210 --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  lock)
+    exec swaylock;;
+  logout)
+    exec hyprctl dispatch exit 0;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
