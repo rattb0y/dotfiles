d2be75ee974672086efb651b6338d9e6fa57ffab
Kris <136197728+rattb0y@users.noreply.github.com>
2024-02-25 16:16:17 -0500

    Delete .config/google-chrome directory

--- .config/google-chrome/Default/Extensions/liecbddmkiiihnedobmlmillhodjkdmb/5.5.47_2/js/embedView.js
+++
@@ -1,5 +0,0 @@
-
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="ad5217ce-cc98-5214-8581-c87d29ca08ec")}catch(e){}}();
-!function(){"use strict";chrome.runtime.id;const e=document.createElement("style");e.append("\n  html,\n  body {\n    overflow: hidden;\n  \n    margin: 0;\n    width: 100%;\n    height: 100%;\n    padding: 0;\n  \n    background: transparent;\n  }\n  \n  iframe {\n    border: none;\n    width: 100%;\n    height: 100%;\n  }\n  "),document.head.append(e);const n=document.createElement("iframe"),t=((e,n)=>{e=e.replace(/[[]/,"\\[").replace(/[\]]/,"\\]");const t=document.createElement("a");t.href=n;const o=new RegExp("[\\?&]"+e+"=([^&#]*)").exec(t.search);return null===o?"":o[1].replace(/\+/g," ")})("session_id",window.location.href);n.allowFullscreen=!0,n.frameBorder="0",n.src=`https://www.loom.com/embed/${t}?from_link_expand=true&${window.location.search}`,document.body.appendChild(n)}();
-//# sourceMappingURL=embedView.js.map
-//# debugId=ad5217ce-cc98-5214-8581-c87d29ca08ec
